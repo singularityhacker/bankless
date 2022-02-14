ce197134352d4e03502505e3b8af2517b0d0d8c1
Germain Boue <boue.germain@gmail.com>
2022-02-13 18:33:01 -0800

    home && about us

--- components/HomeOne/MainBanner.js
+++ components/HomeOne/MainBanner.js
@@ -8,22 +8,10 @@ const MainBanner = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-6">
                         <div className="banner-text">
-                            <h1>Specialized Artificial Intelligence Startup</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida risus commodo</p>	
+                            <h1>BanklessDAO</h1>
+                            <p>Media and social DAO onboarding 1 billion people to crypto. </p>	
 
-                            <div className="banner-btn">
-                                <Link href="/about-1">
-                                    <a className="default-btn">
-                                        Learn More
-                                    </a>
-                                </Link>
-
-                                <Link href="/contact">
-                                    <a className="default-btn">
-                                        Contact Us
-                                    </a>
-                                </Link>
-                            </div>
+                            
                         </div>
                     </div>
 
